bbc2c638f6efb466cde8bed1248866f7c083e6f0
aliza <alizairani@gmail.com>
2018-09-25 20:05:05 +0300

    printScreen and file to crate sql

---
+++ filesGameProject/SQLQuery2.sql
@@ -0,0 +1,9 @@
+use GameDB
+insert  into Games values ('chess', 'Ram','Ran','Ran')
+insert  into Games values ('chess', 'Or','keren','Or')
+insert  into Games values ('chess', 'Bery','Beni','Beni')
+insert  into Games values ('checkers', 'Rachel','Ran','Rachel')
+insert  into Games values ('checkers', 'Avi','Ami','Ami')
+insert  into Games values ('checkers', 'Ami','Ran','Ran')
+insert  into Games values ('taki', 'Or','Rachel','Or')
+insert  into Games values ('taki', 'Beni','Avi','Avi')
